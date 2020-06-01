66110b5614413fe15c93df6855ca3e2ced6c6660
Rowland Ogwara <rogwara@uchicago.edu>
2020-06-01 16:26:33 -0500

    pip compile dependencies

--- setup.py
+++ setup.py
@@ -10,19 +10,16 @@ setup(
         ]
     },
     install_requires=[
-        'flask<2',
-        'jsonschema<3',
-        'sqlalchemy==1.3.3',
-        'sqlalchemy-utils<1',
-        'psycopg2>=2.7',
-        'cdislogging',
-        'indexclient',
-        'doiclient',
-        'dosclient',
-        'future<1'
-    ],
-    dependency_links=[
-        "git+https://github.com/uc-cdis/cdislogging.git@0.0.2#egg=cdislogging",
-        "git+https://github.com/NCI-GDC/indexclient.git@1.5.10#egg=indexclient",
+        'flask~=1.1',
+        'jsonschema~=3.2',
+        'sqlalchemy~=1.3',
+        'sqlalchemy-utils~=0.36',
+        'psycopg2~=2.8',
+        'future~=0.18',
+        'Werkzeug~=0.16',
+        'cdislogging @ git+https://github.com/uc-cdis/cdislogging.git@0.0.2#egg=cdislogging',
+        'indexclient @ git+https://github.com/NCI-GDC/indexclient.git@1.5.10#egg=indexclient',
+        'doiclient @ git+https://github.com/uc-cdis/doiclient.git@1.0.0#egg=doiclient',
+        'dosclient @ git+https://github.com/uc-cdis/dosclient.git@1.0.0#egg=dosclient',
     ],
 )
